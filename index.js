de213598b63912612a976ac10c43577ec57727e2
Kemal09 <86905661+Kemal09@users.noreply.github.com>
2022-07-28 12:39:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log(31)
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+})
+
+app.get("/", (req, res) => res.send("Okey"))
+
+server.listen(3000, "0.0.0.0", () => console.log("Server started")
